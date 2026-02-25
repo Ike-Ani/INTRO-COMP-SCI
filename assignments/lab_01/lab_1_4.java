bbcb257243caffb8978241c59ae76bce17b641f1
Ikechukwu Ani <newikejr@gmail.com>
2026-02-25 16:39:26 -0500

    Add files via upload

---
+++ assignments/lab_01/lab_1_4.java
@@ -0,0 +1,6 @@
+public class lab_1_4 {
+    public static void main(String[] args) {
+        System.out.println("__________________________\n\tGrocery Receipt\n__________________________");
+        System.out.println("item\tPrice\nApples\t$2.50\nBananas\t$1.50\nOranges\t$3.10");
+    }
+}
