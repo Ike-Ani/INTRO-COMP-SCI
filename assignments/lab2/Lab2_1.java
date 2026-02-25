a2d39ba4baa7a0121e25db8a618235f44300b8cb
Ikechukwu Ani <newikejr@gmail.com>
2026-02-25 16:56:15 -0500

    Add files via upload

---
+++ assignments/lab2/Lab2_1.java
@@ -0,0 +1,20 @@
+// **********************************************
+
+// COSC 236                LAB #2
+
+// YOUR NAME: Ikechukwu Ani
+
+// DUE-DATE: 02/20/2026
+
+// PROGRAM-NAME: Lab2_1
+
+// Print out the text "Hello, World!"
+
+//**********************************************
+
+public class Lab2_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
---
+++ assignments/lab2/Lab2_2.java
@@ -0,0 +1,26 @@
+//***********************************************
+
+// COSC 236                LAB #2
+
+// YOUR NAME: Ikechukwu Ani
+
+// DUE-DATE: 02/20/2025
+
+// PROGRAM-NAME: Lab2_2
+
+// Compute the sum of 2 integers
+
+//***********************************************
+
+public class Lab2_2 {
+    public static void main(String[] args) {
+
+        int x,y,sum;
+        x = 18;
+        y = 280;
+        sum = x + y;
+
+        System.out.println("Let's do some simple math!");
+        System.out.println("The sum of " + x + " and " + y + " = " + sum);
+    }
+}
